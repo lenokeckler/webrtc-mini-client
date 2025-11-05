2b208f6accb13610f78696279dbd3311a9f3cbee
Leno <144745403+lenokeckler@users.noreply.github.com>
2025-11-05 15:07:52 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  WEBSOCKET_URL: 'wss://webrtc-mini-server.azurewebsites.net'
+  WEBSOCKET_URL: 'wss://webrtc-mini-server-a4dhd6gah7d0aye2.eastus-01.azurewebsites.net'
 };
 export default config;
